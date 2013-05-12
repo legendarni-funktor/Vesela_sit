7edec5667440bd6e3fdee3e8f68cef1aef8b556b
Moutes <tomasm5@atlas.cz>
2013-05-12 17:09:28 +0200

    krok zpet

--- classes_1.py
+++ classes_1.py
@@ -195,8 +195,6 @@ print "Preprocessing successfully finished!\n"
 print "Getting clever!..."
 error_plot = Plot()
 
-print training_set[0]
-
 for i in xrange(100):
     current_error = net.net_error()
     error_plot.update(current_error)
