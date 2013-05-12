9c7a06914efb1fab5a68fd687ae686e68a41d356
Pedro <plhon1@seznam.cz>
2013-05-12 21:56:47 +0200

    Revert "Functioning version, corrected error in preprocess - the expected output in store set was not represented as element of a list."
    This reverts commit af4ece316f02f2f998eec1f92105ada4b0d5b221.

--- preprocess.py
+++ preprocess.py
@@ -109,7 +109,7 @@ def preproces_set(store_set, path, output, columns, rows):
 				up = vyska_dilku*j + horni_bod + okraj
 				x = sirka_dilku*(i+1) + levy_bod - okraj
 				y = vyska_dilku*(j+1) + horni_bod - okraj
-				store_set.append([cut(obrazek, pix, (left, up), (x, y)), (output,)])
+				store_set.append([cut(obrazek, pix, (left, up), (x, y)), output])
 
 def cut(obrazek, pix, (left,up), (x,y)):	
 	hranice = 760
@@ -177,14 +177,14 @@ def cut(obrazek, pix, (left,up), (x,y)):
 	pravy_bod = x
 	dolni_bod = y	
 	
-#	print (x,y)
-#	print obrazek.size
+	print (x,y)
+	print obrazek.size
 	
 	matice = []
 	sirka_puvodni = pravy_bod - levy_bod
 	vyska_puvodni = dolni_bod - horni_bod	
-	sirka = 30
-	vyska = 20
+	sirka = 70
+	vyska = 40
 	sirka_dilku = sirka_puvodni/sirka + 1
 	vyska_dilku = vyska_puvodni/vyska + 1
 	for i in range(sirka):
