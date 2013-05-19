5bc1c472002492c65af0a0674d07b57e5175cae2
Moutes <tomasm5@atlas.cz>
2013-05-19 21:06:27 +0200

    prilis pomale

--- preprocess.py
+++ preprocess.py
@@ -24,10 +24,10 @@ def normalize_set(path):
 	
 	
 def create_valid_train_preproc_sets(positive, negative, size):
-	for i, path in enumerate(['training_set_img_neg', 'valid_set_img_neg']):
-		preproces_set(negative, path, 0, 7, 25, size)
-	for i, path in enumerate(['valid_set_img_pos', 'training_set_img_pos']):
-		preproces_set(positive, path, 1, 7, 25, size)
+  for i, path in enumerate(['training_set_img_neg', 'valid_set_img_neg']):
+    preproces_set(negative, path, 0, 7, 25, size)
+  for i, path in enumerate(['valid_set_img_pos', 'training_set_img_pos']):
+    preproces_set(positive, path, 1, 7, 25, size) 
 		
 
 def preproces_set(store_set, path, output, columns, rows, size):
@@ -96,98 +96,140 @@ def preproces_set(store_set, path, output, columns, rows, size):
 			(r,g,b) = pix[p1x,p1y]
 		pravy_bod = p1x
 		
-		okraj = 50
+		okraj = 30
 		okraj = (obrazek.size[0] * okraj)/7000
 		
-		sirka_puvodni = pravy_bod - levy_bod
-		vyska_puvodni = dolni_bod - horni_bod
-		sirka_dilku = sirka_puvodni/columns - 1
-		vyska_dilku = vyska_puvodni/rows - 1
-		for i in xrange(columns):
-			for j in xrange(rows):				
-				left = sirka_dilku*i + levy_bod + okraj
-				up = vyska_dilku*j + horni_bod + okraj
-				x = sirka_dilku*(i+1) + levy_bod - okraj
-				y = vyska_dilku*(j+1) + horni_bod - okraj
-				orez = cut(obrazek, pix, (left, up), (x, y), size)
-				if orez:
-					store_set.append((orez, (output,)))
+ 		sirka_puvodni = pravy_bod - levy_bod
+ 		vyska_puvodni = dolni_bod - horni_bod
+ 		sirka_dilku = sirka_puvodni/columns
+ 		vyska_dilku = vyska_puvodni/rows
+ 		for i in xrange(columns):
+ 			for j in xrange(rows):
+ 				left = sirka_puvodni*i/columns + levy_bod + okraj
+ 				#up = vyska_dilku*j + horni_bod + okraj
+ 				up = vyska_puvodni*j/rows + horni_bod + okraj
+ 				#x = sirka_dilku*(i+1) + levy_bod - okraj
+ 				x = sirka_puvodni*(i+1)/columns + levy_bod - okraj
+ 				#y = vyska_dilku*(j+1) + horni_bod - okraj
+ 				y = vyska_puvodni*(j+1)/rows + horni_bod - okraj
+ 				orez = cut(obrazek, pix, (left, up), (x, y), size)
+ 				if orez:
+ 					store_set.append((orez, (output,)))
 	
-		for k in range(x):
-			pix[k, horni_bod] = (0,255,0)				
-#		obrazek.show()
+
+		
+# 		for k in range(x):
+# 			pix[k, horni_bod] = (0,255,0)				
+		obrazek.show()
 
 def cut(obrazek, pix, (left,up), (x,y), size):	
 	hranice = 760
 	left = left + (x - left)/10
 	x = x - (x - left)/10
-	#hledani horniho bodu 
-	i = left
-	j = up
-	citlivost_r = 200
-	citlivost_b = 300
-	#rozsah = (x - left)/300
-	rozsah = 1	#vic zpomaluje a orezava moc
-	(r,g,b) = pix[i,j]
-	while b<citlivost_b or r>citlivost_r:
-		if i == x-1:
-			i = left
-			j = j+1
-			if j == y:
- 				print "prazdny obrazek"
-				return False
-		else:
-			i = i+1
-		(r,g,b) = pix[i,j]
-		r += redness(pix, i, j, rozsah)
-		b += blueness(pix, i, j, rozsah)
-	horni_bod = j
-	i = x-2
-	j = y-2
-	while b<citlivost_b or r>citlivost_r:
-		if i == left:
-			i = x-2
-			j = j-1
-			if j == 0:
- 				print "prazdny obrazek"
-				return False
-		else:
-			i = i-1
-		(r,g,b) = pix[i,j]
-		r += redness(pix, i, j, rozsah)
-		b += blueness(pix, i, j, rozsah)
-	dolni_bod = j
-	i = left
-	j = up
-	while b<citlivost_b or r>citlivost_r:
-		if j == y-1:
-			j = up
-			i = i+1
-			if i == x:
- 				print "prazdny obrazek"
-				return False
-		else:
-			j = j+1
-		(r,g,b) = pix[i,j]
-		r += redness(pix, i, j, rozsah)
-		b += blueness(pix, i, j, rozsah)
-	levy_bod = i
-	i = x-2
-	j = y-2
-	while b<citlivost_b or r>citlivost_r:
-		if j == up:
-			j = y-2
-			i = i-1
-			if i == 0:
- 				print "prazdny obrazek"
-				return False
-		else:
-			j = j-1
-		(r,g,b) = pix[i,j]
-		r += redness(pix, i, j, rozsah)
-		b += blueness(pix, i, j, rozsah)
-	pravy_bod = i
+	citlivost_r = 290
+	citlivost_b = 180
+	citlivost = 0.05
+	horni_bod = up
+	dolni_bod = y
+	levy_bod = left
+	pravy_bod = x
 	
+	mezisoucet = 0
+	while float(mezisoucet)/((pravy_bod-levy_bod)*(dolni_bod-horni_bod)) < citlivost:
+		mezisoucet = 0	
+		for i in xrange(levy_bod, pravy_bod):
+			for j in xrange(horni_bod,dolni_bod):
+ 				(r,g,b) = pix[i,j]
+ 				r += redness(pix, i, j)
+ 				b += blueness(pix, i, j)				
+				if (b>citlivost_b and r<citlivost_r):
+					mezisoucet += 1
+		citlivost_r += 10
+		citlivost_b -= 10
+		
+	citlivost_r -= 10
+	citlivost_b += 10
+	
+	#hledani horniho bodu 
+	i = levy_bod
+	j = horni_bod
+	(r,g,b) = pix[i,j]
+	r += redness(pix, i, j)
+	b += blueness(pix, i, j)
+	while (b<citlivost_b or r>citlivost_r):
+		if i == pravy_bod - 1:
+			i = levy_bod
+			j = j+1
+			if j == dolni_bod:
+ 				print "prazdny obrazek"
+				return False
+		else:
+			i = i+1
+		(r,g,b) = pix[i,j]
+		r += redness(pix, i, j)
+		b += blueness(pix, i, j)
+	horni_bod = j
+	
+	#hledani dolniho bodu
+	i = pravy_bod - 2
+	j = dolni_bod - 2
+	(r,g,b) = pix[i,j]
+	r += redness(pix, i, j)
+	b += blueness(pix, i, j)
+	while b<citlivost_b or r>citlivost_r:
+		if i == levy_bod:
+			i = pravy_bod - 2
+			j = j - 1
+			if j == horni_bod:
+ 				print "prazdny obrazek"
+				return False
+		else:
+			i = i-1
+		(r,g,b) = pix[i,j]
+		r += redness(pix, i, j)
+		b += blueness(pix, i, j)
+	dolni_bod = j
+	
+	#hledani leveho bodu
+	i = levy_bod
+	j = horni_bod
+	(r,g,b) = pix[i,j]
+	r += redness(pix, i, j)
+	b += blueness(pix, i, j)
+	while b<citlivost_b or r>citlivost_r:
+		if j == dolni_bod - 1:
+			j = horni_bod
+			i = i+1
+			if i == pravy_bod:
+ 				print "prazdny obrazek"
+				return False
+		else:
+			j = j+1
+		(r,g,b) = pix[i,j]
+		r += redness(pix, i, j)
+		b += blueness(pix, i, j)
+	levy_bod = i
+	
+	#hledani praveho bodu
+	i = pravy_bod - 2
+	j = dolni_bod - 2
+	(r,g,b) = pix[i,j]
+	r += redness(pix, i, j)
+	b += blueness(pix, i, j)
+	while b<citlivost_b or r>citlivost_r:
+		if j == horni_bod:
+			j = dolni_bod - 2
+			i = i - 1
+			if i == levy_bod:
+ 				print "prazdny obrazek"
+				return False
+		else:
+			j = j-1
+		(r,g,b) = pix[i,j]
+		r += redness(pix, i, j)
+		b += blueness(pix, i, j)
+	pravy_bod = i
+		
 	matice = []
 	sirka_puvodni = pravy_bod - levy_bod
 	vyska_puvodni = dolni_bod - horni_bod	
@@ -206,14 +248,22 @@ def cut(obrazek, pix, (left,up), (x,y), size):
 			a = barva/(sirka_dilku * vyska_dilku)
 			matice[i].append(a)
 	
-	#zeleny obdelnicek pro debug
-# 	for i in range(levy_bod, pravy_bod):
-# 		pix[i,horni_bod] = (0, 255, 0)
-# 		pix[i,dolni_bod] = (0, 255, 0)
-# 	for i in range(horni_bod, dolni_bod):
-# 		pix[pravy_bod,i] = (0, 255, 0)
-# 		pix[levy_bod,i] = (0, 255, 0)	
+# 	#vykresleni bodu v ramci citlivosti zelelne
+# 	for k in xrange(levy_bod,pravy_bod):
+# 		for l in xrange(horni_bod, dolni_bod):
+# 			(r,g,b) = pix[k,l]
+# 			r += redness(pix, k, l)
+# 			b += blueness(pix, k, l)
+# 			if (b>citlivost_b and r<citlivost_r):
+# 				pix[k,l] = (r,0,b)
 	
+ 	#zeleny obdelnicek pro debug
+  	for i in xrange(levy_bod, pravy_bod):
+  		pix[i,horni_bod] = (0, 255, 0)
+  		pix[i,dolni_bod] = (0, 255, 0)
+  	for i in xrange(horni_bod, dolni_bod):
+  		pix[pravy_bod,i] = (0, 255, 0)
+  		pix[levy_bod,i] = (0, 255, 0)	
 	
 	#prepsani matice na cernobilou
 	for i in xrange(len(matice)):
@@ -239,16 +289,8 @@ def cut(obrazek, pix, (left,up), (x,y), size):
  	
 	return output_vector
 
-def redness(pix, x, y, rozsah):
-	r = 0
-	for i in range(x-rozsah,x+rozsah):
-		for j in range(y-rozsah,y+rozsah):
-			r += pix[i,j][0]
-	return r / (2*rozsah + 1)**2
+def redness(pix, i, j):
+	return (pix[i,j][0] + pix[i+1,j][0] + pix[i+1,j+1][0] + pix[i,j+1][0] + pix[i-1,j+1][0] + pix[i-1,j][0] + pix[i-1,j-1][0] + pix[i,j-1][0] + pix[i+1,j-1][0])/9
 
-def blueness(pix, x, y, rozsah):
-	r = 0
-	for i in range(x-rozsah,x+rozsah):
-		for j in range(y-rozsah,y+rozsah):
-			r += pix[i,j][2]
-	return r / (2*rozsah + 1)**2
+def blueness(pix, i, j):
+	return  (pix[i,j][2] + pix[i+1,j][2] + pix[i+1,j+1][2] + pix[i,j+1][2] + pix[i-1,j+1][2] + pix[i-1,j][2] + pix[i-1,j-1][2] + pix[i,j-1][2] + pix[i+1,j-1][2])/9
